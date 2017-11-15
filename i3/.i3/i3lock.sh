636f6df1839fd8535d9009894c2fccabeac9f695
Robert James Hernandez <rob@sarcasticadmin.com>
2017-11-14 16:59:35 -0800

    Testing locking with USR1

--- i3/.i3/i3lock.sh
+++ i3/.i3/i3lock.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 scrot /tmp/screen_locked.png
 convert /tmp/screen_locked.png -blur 8x8 /tmp/screen_locked2.png
-i3lock -i /tmp/screen_locked2.png
+i3lock -e -f -i /tmp/screen_locked2.png
+xset dpms force off
+doas killall -u root -USR1 moused
